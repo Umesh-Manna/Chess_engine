7035134022c043a93e7eea40f3c9a415529da955
Umesh-Manna <ume.man.rt24@dypatil.edu>
2026-06-08 23:51:36 +0530

    moved the main code to board.cpp, instead of main.cpp

--- engine/src/board.cpp
+++ engine/src/board.cpp
@@ -1,1 +1,47 @@
-//this file is for creating a board. 
+//chess engine day one
+//no point in thinking too much, let's just begin
+//orginally this was in main.cpp, but i think this file is better suited for board.cpp
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+
+
+
+
+class Move {
+    int from_square;
+    int to_squuare; 
+};
+
+
+class ChessBoard {
+public:
+    void loadFromFEN(const std::string& fen) {
+        //todo : parse the fen string
+    }
+
+public:
+    void printBoard() {
+        //todo : Print 8*8 board using terminal using letters
+
+    }
+
+public: 
+    std::vector<Move> generatePseudoLegalMoves() {
+        std::vector<Move> moves; 
+        //todo: loop through the sqares, find pieces and calculate where they go....
+        return moves;        
+    }
+
+};
+
+int main() {
+    ChessBoard board;
+    //this is the starting position of the board..
+    board.loadFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    board.printBoard();
+    return 0 ;
+}
